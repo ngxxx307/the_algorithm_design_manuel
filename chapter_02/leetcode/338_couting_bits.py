cbb0ff960caa18c0dce973cc5c45d80a70f4a691
kurtBlockAI <kurt@blockchainaitech.co>
2025-12-02 13:42:15 +0800

    WIP

---
+++ chapter_02/leetcode/338_couting_bits.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def countBits(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        i = 0
+        power = 0
+        ans = []
+        
+        for i in range(n):
+            
+        
+        
